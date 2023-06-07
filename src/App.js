e9d6dec26909771984eb0c17842b8221cfcd23ef
rizwan4229 <malikrizvi41@gmail.com>
2023-06-07 07:43:04 +0000

    change app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js PascalineSoft application</code> and save to reload.
+          Edit <code>src/App.js PascalineSoft IT Company</code> and save to reload.
         </p>
         <a
           className="App-link"
