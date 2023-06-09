181c15810af519c9df6f752e41ef44b65579bc21
Rizwan4229 <malikrizvi41@gmail.com>
2023-06-09 09:28:34 +0000

    new push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const isDocker = process.env.REACT_APP_IS_DOCKER === 'true';
+
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      {isDocker ? <p>Running inside Docker</p> : <p>Running on local pascaline system</p>}
+    </div>
+  );
+}
+
+export default App;
